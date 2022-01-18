c44cb944989a9214731e13de4f0e66594218037f
iñigo <igoikoetxeal@alumnos.nebrija.es>
2022-01-18 22:55:11 +0100

    Initial commit

---
+++ MediaMedianaVarianza.py
@@ -0,0 +1,6 @@
+Import stats_data as st
+res = sd.StatsData([1,4,8.5,3.1])
+print("Lista de números: ",res.l_data)
+print("Media: ",res.mean)
+print("Mediana; ",res.median)
+print("Varianza: ",res.variance)
